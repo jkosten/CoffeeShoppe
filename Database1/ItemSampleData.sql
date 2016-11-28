b01f9a4dd917550897da692b46e02aa4c9bddc18
Doreen <ddtroyer@gmail.com>
2016-11-28 11:23:41 -0500

    Add test cases

---
+++ Database1/ItemSampleData.sql
@@ -0,0 +1,8 @@
+﻿USE [CoffeeShoppeProducts]
+GO
+INSERT [dbo].[MenuItems] ([Id], [Category], [Description], [Price], [Availability], [Detailed Description]) VALUES (1, N'Coffee', N'3 lb. Columbian', 15.0000, 1, N'Fresh from the rainforests of Columbia.')
+GO
+INSERT [dbo].[MenuItems] ([Id], [Category], [Description], [Price], [Availability], [Detailed Description]) VALUES (2, N'Coffee', N'5 lb. Columbian', 22.0000, 0, N'Fresh from the Columbian rainforest.')
+GO
+INSERT [dbo].[MenuItems] ([Id], [Category], [Description], [Price], [Availability], [Detailed Description]) VALUES (3, N'Food', N'Bagel', 2.5000, 1, N'Sesame seeds aren''t free.')
+GO
