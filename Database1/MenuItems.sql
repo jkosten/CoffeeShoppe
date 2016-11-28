0474051ff6c57e7aabd8a37108624d6a2624c62a
Doreen <ddtroyer@gmail.com>
2016-11-28 11:12:14 -0500

    Add database project

---
+++ Database1/MenuItems.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[MenuItems]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Category] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(210) NOT NULL, 
+    [Price] SMALLMONEY NOT NULL, 
+    [Availability] BIT NOT NULL, 
+    [Detailed Description] NVARCHAR(500) NULL
+)
